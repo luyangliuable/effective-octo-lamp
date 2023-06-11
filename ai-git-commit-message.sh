75f99653bc294d97f4ea65100de5b88daada4be4
Luyang Liu <luyangl16@gmail.com>
2023-06-11 16:11:02 +1000

    Allow autocommiter to detect deleted and new files.

--- ai-git-commit-message.sh
+++ ai-git-commit-message.sh
@@ -1,38 +1,62 @@
 function generate_git_message() {
-    newfiles=$1
+    new_files_output=$(git ls-files --others --exclude-standard)
+
+    new_files=()
+
+    while IFS= read -r line; do
+        new_files+=("$line")
+    done <<< "$new_files_output"
+    
+    # Now loop over the array:
+    new_file_contents=$(for file in "${new_files[@]}"; do
+        if [[ -f $file ]]; then
+            # Print the contents of the file
+            echo "Content of ./$file:"
+            cat "$file"
+            echo "\n"
+        fi
+    done)
 
     modifiedfiles=$(git status | awk '/modified:/ { print $2 }')
 
-    changes=$(git diff --unified=0 | tr -dc '[:alnum:][]()+-. \n')
+    modifiedfilechanges=$(git diff --unified=0 | tr -dc '[:alnum:][]()+-. \n')
 
-    message="Write me a git commit message based on the following:\n\nNewfiles: $newfiles\nFile change: $modifiedfiles \n$changes"
+    removedfiles=$(git status | awk '/deleted:/ { print $2 }')
 
-    python3 << END
-import json
-import requests
+    message="Write me a git commit message based on the following:\n\n\
+Removed files:\n$removedfiles\n\n\
+Newfilechanges: $new_file_contents\n\n\
+File changes: $modifiedfiles \n\
+$modifiedfilechanges"
 
-content = """$message"""
+    echo "$message"
 
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-        {
-            "role": "user", 
-            "content": content
-        }
-    ]
-}
+#     python3 << END
+# import json
+# import requests
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer $OPENAI_API_KEY"
-}
+# content = """$message"""
 
-response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
+# data = {
+#     "model": "gpt-3.5-turbo",
+#     "messages": [
+#         {
+#             "role": "user", 
+#             "content": content
+#         }
+#     ]
+# }
 
-# print(response.json())
-print(response.json()['choices'][0]['message'][ 'content' ])
-END
+# headers = {
+#     "Content-Type": "application/json",
+#     "Authorization": "Bearer $OPENAI_API_KEY"
+# }
+
+# response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
+
+# # print(response.json())
+# print(response.json()['choices'][0]['message'][ 'content' ])
+# END
 }
 
 generate_git_message
